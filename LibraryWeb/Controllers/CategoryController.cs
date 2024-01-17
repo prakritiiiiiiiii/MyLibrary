43c5718d0b58013bff921b73927effef48f8beb0
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-01-17 21:28:47 +0545

    Add project files.

---
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using LibraryWeb.Data;
+using LibraryWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LibraryWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+     
+           private readonly ApplicationDbContext _db;
+           public CategoryController(ApplicationDbContext db)
+           {
+            _db = db;
+           }
+            public IActionResult Index()
+            {
+             List<Category> objCategoryList = _db.Categories.ToList();  
+              return View(objCategoryList);
+            }
+
+            public IActionResult Create()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Create(Category obj)
+            {
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+              
+                return View();
+            }
+    }
+}
---
+++ LibraryWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Self-help", DisplayOrder = 3 },
+                new Category { Id = 3, Name = "Romance", DisplayOrder = 2 },
+                new Category { Id = 4, Name = "Thriller", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Fantasy", DisplayOrder = 2 }
+
+                );
+        }
+    }
+        
+}
