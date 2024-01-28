80be863c7a620e281cc6bdb4635630fbd7ed3cb6
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-01-28 12:55:25 +0545

    .

--- Library.DataAccess/Repository/Category.cs
+++ Library.DataAccess/Repository/Category.cs
@@ -10,19 +10,22 @@ using System.Threading.Tasks;
 
 namespace Library.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>,ICategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
         private ApplicationDbContext _db;
         public CategoryRepository(ApplicationDbContext db) : base(db)
-        { 
+        {
             _db = db;
         }
 
-        
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
 
         public void Update(Category obj)
         {
-            _db.Categories.Update(obj); 
+            _db.Categories.Update(obj);
         }
     }
 }
