89adeaf2a89345e2d8926efb09aa48fb54dafb32
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-01-30 15:42:43 +0545

    product added

--- Library.Models/Product.cs
+++ Library.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Library.Models
 {
@@ -44,6 +45,17 @@ namespace Library.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }    
+        
+        
+
+
+
         
     }
 }
