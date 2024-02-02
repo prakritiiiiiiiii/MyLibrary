2308ede73debaebea988e92485a35164dca007a1
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-02 21:45:01 +0545

    view model added

---
+++ Library.Models/ProductVm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.Models
+{
+    public class ProductVm
+    {
+
+          
+
+        public Product Product { get; set; }
+        //public int  CategoryId { get; set; }
+        public List<Category> CategoryList { get; set; }
+
+        public SelectList getCategories() => new SelectList(CategoryList,nameof(Category.Id), nameof(Category.Name), Product.CategoryId);
+        public Category Category { get; set; }
+        //public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+    }
+}
