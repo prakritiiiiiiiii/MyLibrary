91055ab32cf5c510dbdcbddd73f76541becab431
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-02 21:45:55 +0545

    category view model

---
+++ LibraryWeb/Areas/Admin/ViewModel/CategoryIndexVm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWeb.Areas.Admin.ViewModel
+{
+    public class CategoryIndexVm
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        public List<CategoryInfoVm> Categories { get; set; }
+    }
+    public class CategoryInfoVm
+    {
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
