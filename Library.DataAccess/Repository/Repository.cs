17983657a7a0bcda0c46e271099cb846b118e8dd
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-03 21:13:22 +0545

    repo

--- Library.DataAccess/Repository/Repository.cs
+++ Library.DataAccess/Repository/Repository.cs
@@ -15,65 +15,43 @@ namespace Library.DataAccess.Repository
     public class Repository<T> : IRepository<T> where T : class
     {
 
-        private readonly DbSet<T> _dbSet;
-        private readonly DbContext _context;
 
-        //public Repository(ApplicationDbContext db)
-        //{
-        //    _db = db;  
-            
-        //  this._dbSet = db.Set<T>();
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
 
-        //    //_db.Categories == dbSet
-        //}
-
-        public Repository(DbContext context)
+        public Repository(ApplicationDbContext db)
         {
-            _context = context;
-            _dbSet = _context.Set<T>();
-        }
+            _db = db;
+            this.dbSet = _db.Set<T>();
 
+        }
         public void Add(T entity)
         {
-            _dbSet.Add(entity);  
+            dbSet.Add(entity);
         }
 
         public T Get(Expression<Func<T, bool>> filter)
         {
-            IQueryable<T> query = _dbSet;
-            query= query.Where(filter);
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
             return query.FirstOrDefault();
         }
 
         public IEnumerable<T> GetAll()
         {
-            IQueryable<T> query = _dbSet;
+            IQueryable<T> query = dbSet;
             return query.ToList();
-        }
 
-        public Task<List<T>> GetAllAsync()
-        {
-            return _db.Set<T>().ToListAsync();
         }
 
         public void Remove(T entity)
         {
-            _dbSet.Remove(entity);
+            dbSet.Remove(entity);
         }
 
-        public void RemoveRange(IEnumerable<T> entity)
+        public void RemoveRange(T entity)
         {
-            _dbSet.RemoveRange(entity);
-        }
-
-        public void Save()
-        {
-            _db.SaveChanges();
-        }
-
-        public void Update(Category obj)
-        {
-            _db.Categories.Update(obj);
+            dbSet.RemoveRange(entity);
         }
     }
 }
