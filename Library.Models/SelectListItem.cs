f8e0844df7a453e6ef98d633f9d7c3739c2f95ed
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-05 12:04:30 +0545

    .

---
+++ Library.Models/SelectListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class SelectListItem
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public object Text { get; set; }
+        public object Name { get; set; }
+    }
+}
