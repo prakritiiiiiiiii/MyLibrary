066101b4d77d50623bec24909f3ec2a616750bea
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-05 12:04:05 +0545

    ,

---
+++ Library.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Library.DataAccess.Data;
+using Library.DataAccess.Repository.IRepository;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(db);
+            Product = new ProductRepository(db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
