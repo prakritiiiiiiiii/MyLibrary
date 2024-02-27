647823d3369eeef2add33d643172f2418c05445a
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-02-27 22:01:08 +0545

    viewmodelllll

---
+++ LibraryWeb/Areas/Admin/ViewModel/DashboardVm.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryWeb.Areas.Admin.ViewModel
+{
+    public class DashboardVm
+    {
+        public int CategoryCount { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
