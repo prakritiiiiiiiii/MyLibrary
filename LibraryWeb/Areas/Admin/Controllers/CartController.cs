621f4601a93a546b9ff1d2d5b5d443c34327573e
Prakriti-git <prakreetiuprety.58@gmail.com>
2024-03-01 21:22:00 +0545

    c

---
+++ LibraryWeb/Areas/Admin/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWeb.Areas.Admin.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
